cbb5ed67e73ead95c6c39af316df2448940aaf63
solevicinity <michaelt.abayneh@gmail.com>
2021-10-29 19:41:18 -0400

    got vid_response working

--- vid_class.py
+++ vid_class.py
@@ -1,22 +1,23 @@
 from youtube import youtube
 import re
 
+
 # Video class
 class Video:
-    def __init__(self, ids):
-        self.ids = ids
+    def __init__(self, vid_id):
+        self.vid_id = vid_id
         vid_req = youtube.videos().list(
             part="contentDetails, snippet, statistics",
-            id=self.ids
+            id=self.vid_id
         )
         global vid_resp
         vid_resp = vid_req.execute()
 
-    def get_entire_response(self):
+    def get_response(self):
         return vid_resp
 
-    def get_video_id(self):
-        return self.ids
+    def get_vid_id(self):
+        return self.vid_id
 
     def get_channel_id(self):
         return vid_resp["items"][0]["snippet"]["channelId"]
@@ -27,7 +28,8 @@ class Video:
     def get_duration(self):
         pass
 
+
 vid1 = Video("19E9RQC5kDo")
 
-output = vid1.get_entire_response()
+output = vid1.get_response()
 print(output)
--- youtube.py
+++ youtube.py
@@ -1,6 +1,6 @@
 from googleapiclient.discovery import build
 
-with open("../Other/api_keys.txt", "r") as f:
+with open("../Other/api_keys.txt", "r", encoding="utf-8") as f:
     api_key = f.read()
 
 youtube = build("youtube", "v3", developerKey=api_key)
