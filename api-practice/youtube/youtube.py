31453b98081024d5eedaa4e2366724d3c18abd2b
solevicinity <michaelt.abayneh@gmail.com>
2021-11-05 22:25:52 -0400

    rewrote the youtube api key path

--- api-practice/youtube/youtube.py
+++ api-practice/youtube/youtube.py
@@ -1,6 +1,8 @@
 from googleapiclient.discovery import build
 
-with open("../Other/api_keys.txt", "r", encoding="utf-8") as f:
+PATH = "../../../Other/youtube_keys.txt"
+
+with open(PATH, "r", encoding="utf-8") as f:
     api_key = f.read()
 
 youtube = build("youtube", "v3", developerKey=api_key)
