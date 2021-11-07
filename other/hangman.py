e8122b2283c4a7a923c2eb05d9c109fbf1ced6e7
solevicinity <michaelt.abayneh@gmail.com>
2021-11-07 12:35:27 -0500

    got user correct incorrect boolean flag working

--- other/hangman.py
+++ other/hangman.py
@@ -2,6 +2,7 @@
 from words_list import words
 from word_state import word_state
 import random
+import string
 
 # Get a valid word from words list
 def get_valid_word():
@@ -15,37 +16,42 @@ def get_valid_word():
 # Variables related to the word and user attempts
 word = get_valid_word()
 word_guess = []
-alphabet = list("abcdefghijklmnopqrstuvwxyz")
+alphabet = list(string.ascii_lowercase)
 attempts_left = 6
 correct_input = False
+input_list = []
 
 # Instructions
 print(
     f"\nWelcome the game of hangman. Each time this script is run, a new word will be generated for you to guess.\nThis script is case insensitive. Type (qq) to exit the script.\nFor this script the word you are looking for a word that contains {len(word)} letters."
 )
 
+# Initial state of the word - filled with hyphens
 for i in enumerate(word):
     word_guess.append("-")
 
 # Print start state
-print("Options: " + ", ".join(alphabet))
 print(word)
 print("".join(word_guess))
 
 
 # Game Logic
 def hangman():
-    word_state(user_input, word, word_guess, correct_input)
-    print(correct_input)
+    # Correct or incorrect guess bool value
+    global correct_input, attempts_left
+    correct_input = word_state(user_input, word, word_guess)
+
+    # Printing the current state
     print("".join(word_guess))
 
-    global attempts_left
-
+    # Taking attempts when wrong guess
     if not correct_input:
         attempts_left -= 1
 
+    # Displaying the amount of attempts left
     print(f"Attempts left: {attempts_left}")
 
+    # Ending the game once attempt equals 0
     if attempts_left == 0:
         print("End of game")
         quit()
@@ -53,8 +59,14 @@ def hangman():
 
 # Game loop
 while True:
-    user_input = input("Guess: ")
+    # Continuous user input
+    user_input = input("\nGuess: ").lower()
+    input_list.append(user_input)
+    print(f"Used letters: {', '.join(set(input_list))}")
+
+    # Quiting game if user input equals (qq)
     if user_input == "qq":
         quit()
+    # If user input is valid, run game logic
     elif user_input in alphabet:
         hangman()
--- other/word_state.py
+++ other/word_state.py
@@ -1,5 +1,14 @@
-def word_state(letter, word, array, correct):
+def word_state(letter, word, array):
+    global current
+    array_len = [set(array), ""]
+
     list_word = list(word)
     for index, element in enumerate(list_word):
         if element == letter:
             array[index] = letter
+
+    array_len[1] = set(array)
+    if array_len[0] != array_len[1]:
+        return True
+    else:
+        return False
